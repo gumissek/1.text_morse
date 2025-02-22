50b40ec27d2fefd04c9d90b8c728ce5879956276
PiotrekB <bilinski.piotr89@gmail.com>
2025-02-22 11:48:09 +0100

    add function to quit and print word in morse

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from traceback import print_tb
 
 chars  = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 morse = {
@@ -12,12 +13,22 @@ morse = {
 }
 
 
-morse_word=''
-slowo  = 'ala ma kota'
-for char in slowo:
-    if char ==' ':
-        morse_word+='  '
+keep_continue=True
+while keep_continue:
+    word = input('Type word or statement to translate into morse (only letters/numbers), if you want to exit type q: ')
+    if word!='q':
+        morse_word=''
+
+        for char in word:
+            if char ==' ':
+                 morse_word+='    '
+            else:
+                morse_word+=morse[char.upper()]
+                morse_word+=' '
+        print(f'Here is your word/statement: \nYour word: {word}\nIn morse: {morse_word}')
+
     else:
-        morse_word+=morse[char.upper()]
-        morse_word+=' '
-print(morse_word)
+        keep_continue=False
+
+
+
